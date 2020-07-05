559606f4df3555bc88b03537dee4614ff026d276
TrayNix <49353393+TrayNix@users.noreply.github.com>
2020-07-05 12:34:37 +0200

    Update forum.js

--- Commandes/forum.js
+++ Commandes/forum.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 
 module.exports.run = async(client, message, args)=>{
 
-message.channel.send('Voici le post forum de : https://gtacityrp.fr/index.php?threads/crooke-cres-33.24649/page-19#post-273537');
+message.channel.send('Voici le post forum : https://gtacityrp.fr/index.php?threads/crooke-cres-33.24649/page-19#post-273537');
 }
 module.exports.help = {
 name:"forum"
