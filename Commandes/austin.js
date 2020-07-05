acfe58aa90fa4d5c3640bbcff6397aa1d20d4838
TrayNix <49353393+TrayNix@users.noreply.github.com>
2020-07-05 13:07:30 +0200

    Update austin.js

--- Commandes/austin.js
+++ Commandes/austin.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 
 module.exports.run = async(client, message, args)=>{
 
-message.channel.send('Voici la présentation du personnage **Austin** : https://gtacityrp.fr/index.php?threads/%C2%AB-one-for-the-gang-two-for-the-fame-%C2%BB.24998/');
+message.channel.send('Voici la présentation du personnage **Austin** : https://gtacityrp.fr/index.php?threads/austin-vicks-vickers.28394/');
 }
 module.exports.help = {
 name:"austin"
