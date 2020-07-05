837f7b7efd56e1a0892bec674069931af0d4b11d
TrayNix <49353393+TrayNix@users.noreply.github.com>
2020-07-05 16:15:19 +0200

    Update serie.js

--- Commandes/serie.js
+++ Commandes/serie.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 
 module.exports.run = async(client, message, args)=>{
 
-message.channel.send('Voici la série afin de te documenté sur le RP de Gang : https://seriestreaming.co/the-wire-sur-ecoute/saison/1/episode/1/serie-streaming-info la série est aussi diponible sous le nom de **Sur écoute** sur OCS et Canal ');
+message.channel.send('Voici la série afin de te documenter sur le RP de Gang : https://seriestreaming.co/the-wire-sur-ecoute/saison/1/episode/1/serie-streaming-info la série est aussi disponible sous le nom de **Sur écoute** sur OCS et Canal ');
 }
 module.exports.help = {
 name:"serie"
