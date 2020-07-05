e59ee6dfc579a8a7bead6ddb7b7d9c7d8f144c82
TrayNix <49353393+TrayNix@users.noreply.github.com>
2020-07-05 13:04:26 +0200

    Add files via upload

---
+++ Commandes/cammy.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js')
+
+module.exports.run = async(client, message, args)=>{
+
+message.channel.send('Voici la présentation du personnage **Cammy** ou **Cameron Shaffer** : https://gtacityrp.fr/index.php?threads/%C2%AB-one-for-the-gang-two-for-the-fame-%C2%BB.24998/');
+}
+module.exports.help = {
+name:"cammy"
+}
